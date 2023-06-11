e07b15f95cdf0f5940db1ee5b53c550c806db6ae
Blessed Sibanda <blessedsibanda.spring@gmail.com>
2023-06-11 15:12:53 +0200

    Integration test for book creation

--- src/test/java/com/bladespring/bookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/bladespring/bookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,16 +1,37 @@
 package com.bladespring.bookshop.catalogservice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.bladespring.bookshop.catalogservice.domain.Book;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void createBook() {
+		var expectedBook = Book.of("1231231231", "Title", "Author", 9.99);
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actualBook -> {
+					Assertions.assertThat(actualBook).isNotNull();
+					Assertions.assertThat(actualBook.getIsbn()).isEqualTo(expectedBook.getIsbn());
+				});
+	}
 }
